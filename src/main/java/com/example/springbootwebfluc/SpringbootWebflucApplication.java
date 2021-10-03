2ac70662d2c1de71dcdfaae746f0ca1e2b2a5c80
marszm <mariusz.szmer@gmail.com>
2021-10-03 10:21:10 +0200

    Initial commit

---
+++ src/main/java/com/example/springbootwebfluc/SpringbootWebflucApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebfluc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebflucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebflucApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootwebfluc/SpringbootWebflucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebfluc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebflucApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
