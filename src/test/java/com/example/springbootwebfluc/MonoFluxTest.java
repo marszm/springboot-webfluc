b2320c5c4c21ca413f62ff8a82a0e4bb35ec31c9
marszm <mariusz.szmer@gmail.com>
2021-10-06 22:38:07 +0200

    added Flux test

--- src/test/java/com/example/springbootwebfluc/MonoFluxTest.java
+++ src/test/java/com/example/springbootwebfluc/MonoFluxTest.java
@@ -1,12 +1,14 @@
 package com.example.springbootwebfluc;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 class MonoFluxTest {
 
     @Test
     void testMono() {
+
         Mono<?> monoString = Mono.just("any data type")
                 .then(Mono.error(new RuntimeException("exception occured")))
                 .log(); //publisher
@@ -14,4 +16,14 @@ class MonoFluxTest {
 
     }
 
+    @Test
+    void testFlux() {
+
+        Flux<String> fluxString = Flux.just("spring, spring boot, hibernate")
+                .concatWithValues("AWS")
+                .concatWith(Flux.error(new RuntimeException("exception occured in Flux") ))
+                .log();
+        fluxString.subscribe(System.out::println, throwable -> System.out.println(throwable.getMessage()));
+
+    }
 }
