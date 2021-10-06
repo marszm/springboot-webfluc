1346e3f25d11639d3e333854f15fdc7da471c28e
marszm <mariusz.szmer@gmail.com>
2021-10-06 22:25:01 +0200

    fix sona warnings

--- src/test/java/com/example/springbootwebfluc/MonoFluxTest.java
+++ src/test/java/com/example/springbootwebfluc/MonoFluxTest.java
@@ -3,11 +3,11 @@ package com.example.springbootwebfluc;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
 
-public class MonoFluxTest {
+class MonoFluxTest {
 
     @Test
-    public void testMono() {
-        Mono<String> monoString = Mono.just("any data type"); //publisher
+    void testMono() {
+        Mono<String> monoString = Mono.just("any data type").log(); //publisher
         monoString.subscribe(System.out::println); //subscriber
 
     }
