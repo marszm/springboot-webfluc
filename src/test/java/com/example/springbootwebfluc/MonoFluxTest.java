b27218df255b35e2e16b4769d36e15300baff4fb
marszm <mariusz.szmer@gmail.com>
2021-10-06 22:23:43 +0200

    test Mono in test class (2)

--- src/test/java/com/example/springbootwebfluc/MonoFluxTest.java
+++ src/test/java/com/example/springbootwebfluc/MonoFluxTest.java
@@ -1,11 +1,14 @@
 package com.example.springbootwebfluc;
 
+import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
 
 public class MonoFluxTest {
 
+    @Test
     public void testMono() {
-        Mono<String> monoString = Mono.just("any data type");
+        Mono<String> monoString = Mono.just("any data type"); //publisher
+        monoString.subscribe(System.out::println); //subscriber
 
     }
 
